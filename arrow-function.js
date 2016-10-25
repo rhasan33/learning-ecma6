7bef1258523a078e30a9b7eeeffde436d24f9b07
Rakib Hasan <rakibhasan@Inovios-MacBook-Pro-3.local>
2016-10-25 09:17:12 +0600

    some information updated

--- arrow-function.js
+++ arrow-function.js
@@ -39,10 +39,7 @@ console.log("\n", Force2(5));
 /**
  * here comes the most interesting part
  * you actually can declare your function in a single line.
- * remember two things, never code like that if
- *
- * 1. your function requires more than one params
- * 2. you need multiple lines of code in your function
+ * remember two things, never code like that if you need multiple lines of code in your function
  */
 let Force3 = (m) => m * g;
 
